671d836864044498116c7ec81119b2aa04af6f84
rooklift <16438795+rooklift@users.noreply.github.com>
2025-02-17 01:48:25 +0000

    Update ok.mjs

--- ok.mjs
+++ ok.mjs
@@ -1,7 +1,9 @@
-import ninemodule from "./modules/nine.mjs"			// Imports whatever the default export is.
-import {nine, add_one} from "./modules/nine.mjs"	// Imports specific named exports.
+import ninemodule from "./modules/nine.mjs"				// Imports whatever the default export is.
+import {nine, add_one} from "./modules/nine.mjs"		// Imports specific named exports.
+import * as ninemodule_alt from "./modules/nine.mjs"	// Imports the named exports in an object.
 
 add_one();
 ninemodule.add_one();
+ninemodule_alt.add_one();
 
 console.log(nine);
